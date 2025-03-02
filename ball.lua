5bbe185546c8355d4cb5cd7d0d513ed54c83f5fc
GNamimates <81744903+GNamimates@users.noreply.github.com.>
2025-03-02 09:51:27 +0800

    made stripped log act as pipes, waxed copper trapdoors as fans, show an icon when the ball is invisible, piston pushes ball

--- ball.lua
+++ ball.lua
@@ -26,8 +26,15 @@ local trailZ = Trail.new()
 local TEXTURE_SHADOW = textures["textures.shadow"]
 local SHADOW_RES = TEXTURE_SHADOW:getDimensions()
 local MODEL_SHADOW = models:newPart("shadow","WORLD")
-MODEL_SHADOW:newSprite("board"):setRenderType("BLURRY")
+MODEL_SHADOW:newSprite("shadow"):setRenderType("BLURRY")
 :texture(TEXTURE_SHADOW,SHADOW_RES:unpack()):rot(90,0,0):pos(cfg.RADIUS*16,0.5,cfg.RADIUS*16):scale((SHADOW_RES.x/16)*cfg.RADIUS*2,(SHADOW_RES.x/16)*cfg.RADIUS*2,0)
+
+local TEXTURE_INVISIBLE = textures["textures.invisible"]
+local INVISIBLE_RES = TEXTURE_INVISIBLE:getDimensions()
+local MODEL_INVISIBLE = models:newPart("invisible","WORLD")
+MODEL_INVISIBLE:newSprite("icon"):setRenderType("CUTOUT_EMISSIVE_SOLID")
+:texture(TEXTURE_INVISIBLE,INVISIBLE_RES:unpack()):rot(90,0,0):pos(cfg.RADIUS*16,0,cfg.RADIUS*16):scale((16/INVISIBLE_RES.x)*cfg.RADIUS*2,(16/INVISIBLE_RES.x)*cfg.RADIUS*2,0)
+
 --- caches
 local side2dir = {
    ["north"] = vectors.vec3(0,0,1),
@@ -38,13 +45,39 @@ local side2dir = {
    ["down"]  = vectors.vec3(0,-1,0),
 }
 
-local proxy_shape = {
-	["minecraft:moss_carpet"] = {
+local proxyShapes = {
+	carpet = {
 		{
 			vec(0,-0.1,0),
 			vec(1,0,1),
-		}
-	}
+		},
+	},
+	stripped = function (block, pos)
+		local collision = {}
+		local axis = block.properties.axis
+		local id = block.id
+		if not (axis == "x" or world.getBlockState(pos + vec(-1,0,0)).id == id) then
+			collision[#collision+1] = {vec(0,0,0),vec(-0.05,1,1)} -- -X surface
+		end
+		if not (axis == "x" or world.getBlockState(pos + vec(1,0,0)).id == id) then
+			collision[#collision+1] = {vec(1,0,0),vec(1.05,1,1)} -- +X surface
+		end
+		
+		if not (axis == "y" or world.getBlockState(pos + vec(0,-1,0)).id == id) then
+			collision[#collision+1] = {vec(0,0,0),vec(1,-0.05,1)} -- +Y surface
+		end
+		if not (axis == "y" or world.getBlockState(pos + vec(0,1,0)).id == id) then
+			collision[#collision+1] = {vec(0,1,0),vec(1,1.05,1)} -- -Y surface
+		end
+		
+		if not (axis == "z" or world.getBlockState(pos + vec(0,0,-1)).id == id) then
+			collision[#collision+1] = {vec(0,0,0),vec(1,1,-0.05)} -- +Z surface
+		end
+		if not (axis == "z" or world.getBlockState(pos + vec(0,0,1)).id == id) then
+			collision[#collision+1] = {vec(0,0,1),vec(1,1,1.05)} -- -Z surface
+		end
+		return collision
+	end
 }
 
 
@@ -52,7 +85,7 @@ events.ENTITY_INIT:register(function ()
 	ball.pos = player:getPos():add(0,1,1)
 end)
 
-
+local windSoundCooldown = 0
 
 events.TICK:register(function ()
 	ball.lpos = ball.pos
@@ -68,7 +101,20 @@ events.TICK:register(function ()
 				local bpos = ball.pos:floor() + offset
 				local block = world.getBlockState(bpos)
 				if block:hasCollision() then
-					for key, value in pairs(proxy_shape[block.id] or block:getCollisionShape()) do
+					local shape
+					for match, proxyShape in pairs(proxyShapes) do
+
+						if block.id:find(match) then
+							if type(proxyShape) == "function" then
+								shape = proxyShape(block,bpos)
+							else
+								shape = proxyShape
+							end
+						end
+					end
+					shape = shape or block:getCollisionShape()
+					
+					for key, value in pairs(shape) do
 						blocks[i] = {value[1] + bpos - cfg.RADIUS + cfg.MARGIN, value[2] + bpos + cfg.RADIUS - cfg.MARGIN}
 						blocksRaw[i] = {value[1] + bpos + cfg.MARGIN, value[2] + bpos - cfg.MARGIN}
 						i = i + 1
@@ -92,7 +138,44 @@ events.TICK:register(function ()
 				force = vectors.rotateAroundAxis(45,force,vec(0,1,0))
 			end
 		end
-		ball.vel = ball.vel + force * 0.05
+		ball.vel = ball.vel + force * (ball.vel-force):length() * 0.05
+	end
+	
+	--- Waxed Copper Trapdoors as fans
+	
+	for side, dir in pairs(side2dir) do
+		local block = raycast:block(ball.pos,ball.pos + dir * 5, "COLLIDER")
+		if block.id == "minecraft:waxed_copper_trapdoor" and (((side == "up" or side == "down") and "false" or "true") == block.properties.open) then
+			ball.vel = ball.vel - dir * 0.1
+			windSoundCooldown = windSoundCooldown - 1
+			if windSoundCooldown < 0 then
+				windSoundCooldown = 5
+				sounds:playSound("minecraft:entity.breeze.slide",ball.pos,1,1)
+			end
+		end
+	end
+	
+	-- Moving Piston pushes the ball
+	local block = world.getBlockState(ball.pos)
+	if block.id == "minecraft:moving_piston" then
+		ball.vel = ball.vel + side2dir[block.properties.facing] * 0.5
+	end
+	
+	-- Fluid
+	if #block:getFluidTags() > 0 then
+		if not ball.isUnderwater then
+			ball.isUnderwater = true
+			sounds["minecraft:entity.generic.splash"]:pos(ball.pos):pitch(1):play()
+			for _ = 1, 10, 1 do
+				particles["minecraft:splash"]:pos(ball.pos):spawn()
+			end
+			particles["minecraft:cloud"]:pos(ball.pos):spawn()
+		end
+		ball.vel = ball.vel * 0.7
+	else
+		if ball.isUnderwater then
+			ball.isUnderwater = false
+		end
 	end
 	
 	--- 2nd layer of protection to make sure the ball never falls through the ground
@@ -122,26 +205,6 @@ events.TICK:register(function ()
 	else
 		ball.vel = ball.vel + cfg.GRAVITY
 	end
-	
-	local block = world.getBlockState(ball.pos)
-	if #block:getFluidTags() > 0 then
-		if not ball.isUnderwater then
-			ball.isUnderwater = true
-			sounds["minecraft:entity.generic.splash"]:pos(ball.pos):pitch(1):play()
-			for _ = 1, 10, 1 do
-				particles["minecraft:splash"]:pos(ball.pos):spawn()
-			end
-			particles["minecraft:cloud"]:pos(ball.pos):spawn()
-		end
-		ball.vel = ball.vel * 0.7
-	else
-		if ball.isUnderwater then
-			ball.isUnderwater = false
-		end
-	end
-	
-	
-	
 end)
 
 function ball.update(delta)
@@ -168,6 +231,40 @@ function ball.update(delta)
 	else
 		MODEL_SHADOW:setVisible(true)
 	end
+end
+
+cfg.MODEL_BALL.preRender = function (delta, context, part)
+	local tpos = math.lerp(ball.lpos, ball.pos, delta)
+	local mat = matrices.mat4()
+	local cpos = client:getCameraPos()
+	local dir = tpos - cpos
+	local look = dir:normalized()
+	local block,pos = raycast:block(tpos,cpos,"VISUAL")
+	if (pos-cpos):lengthSquared() > 0.01 then
+		MODEL_INVISIBLE:setPos((tpos - dir*0.9)*16):rot(90+math.deg(-math.asin(look.y)),90-math.deg(math.atan2(dir.z,dir.x))):scale(0.1,0.1,0.1):setVisible(true)
+	else
+		MODEL_INVISIBLE:setVisible(false)
+	end
+end
+
+function ball.forcePing()
+	pings.state(ball.pos,ball.vel)
+end
+
+function pings.state(p,v)
+	ball.pos = p
+	ball.vel = v
+end
+
+if host:isHost() then
+local pingTimer = 0
+events.WORLD_TICK:register(function ()
+	pingTimer = pingTimer + 1
+	if pingTimer > 20 then
+		pingTimer = 0
+		ball.forcePing()
+	end
+end)
 end
 
 return ball
--- controls.lua
+++ controls.lua
@@ -23,13 +23,23 @@ end
 
 local isHighlghitingBall = false
 local isThrown = false
+local thrownTimer = 20
 local thrownTime = 0
 local dragPos
 local key = {
 	use = keybinds:fromVanilla("key.use")
 }
 
-local armVisibility = {}
+
+function pings.shoot(vel)
+	sounds:playSound("minecraft:entity.player.attack.sweep",ball.pos,0.3,1.6)
+	sounds:playSound("minecraft:block.wood.step",ball.pos,1,2)
+	ball.vel = ball.vel + vel
+	dragPos = vec(0,0,0)
+	throw:clear()
+	isThrown = true
+	thrownTime = 0
+end
 
 key.use.press = function ()
 	if isHighlghitingBall then
@@ -39,14 +49,8 @@ end
 
 key.use.release = function ()
 	if isHighlghitingBall then
-		sounds:playSound("minecraft:entity.player.attack.sweep",ball.pos,0.3,1.6)
-		sounds:playSound("minecraft:block.wood.step",ball.pos,1,2)
 		local hit = ((ball.pos - dragPos) * 0.3)
-		ball.vel = ball.vel + hit + vec(0,hit:length()*0.3,0)
-		dragPos = vec(0,0,0)
-		throw:clear()
-		isThrown = true
-		thrownTime = 0
+		pings.shoot(hit + vec(0,hit:length()*0.3,0))
 	end
 end
 events.POST_WORLD_RENDER:register(function (dt)
@@ -75,12 +79,16 @@ events.POST_WORLD_RENDER:register(function (dt)
 		renderer:setRenderHUD(false)
 	end
 	
-	if isThrown and ball.vel:length() < 0.02 then
-		isThrown = false
-		renderer:setCameraPivot()
-		renderer:renderLeftArm()
-		renderer:renderRightArm()
-		renderer:setRenderHUD(true)
+	if isThrown and (ball.lpos-ball.pos):length() < 0.02 then
+		thrownTimer = thrownTimer - 1
+		if thrownTimer < 0 then
+			isThrown = false
+			renderer:setCameraPivot()
+			renderer:renderLeftArm()
+			renderer:renderRightArm()
+			renderer:setRenderHUD(true)
+			thrownTimer = 20
+		end
 	end
 	if isHighlghitingBall then
 		cfg.MODEL_BALL:setPrimaryRenderType("EMISSIVE_SOLID")
